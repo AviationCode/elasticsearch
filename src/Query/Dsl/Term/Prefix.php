a496eeaf8e27f9635586c21be647ed13c1c3d54c
Ken Andries <ken.andries.1992@gmail.com>
2019-12-25 18:10:29 +0100

    Prefix clause

---
+++ src/Query/Dsl/Term/Prefix.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AviationCode\Elasticsearch\Query\Dsl\Term;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+class Prefix implements Arrayable
+{
+    const KEY = 'prefix';
+
+    /**
+     * Field you wish to search.
+     *
+     * @var string
+     */
+    private $field;
+
+    /**
+     * Beginning characters of terms you wish to find in the provided.
+     *
+     * @var mixed
+     */
+    private $value;
+
+    /**
+     * Method used to rewrite the query.
+     *
+     * @var string|null
+     */
+    private $rewrite;
+
+    /**
+     * Prefix constructor.
+     *
+     * @param string $field
+     * @param $value
+     * @param string|null $rewrite
+     */
+    public function __construct(string $field, $value, ?string $rewrite = null)
+    {
+        $this->field = $field;
+        $this->value = $value;
+        $this->rewrite = $rewrite;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toArray()
+    {
+        return [
+            static::KEY => [
+                $this->field => array_merge(['value' => $this->value], array_filter(['rewrite' => $this->rewrite]))
+            ]
+        ];
+    }
+}
---
+++ tests/Unit/Query/Dsl/Term/PrefixTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AviationCode\Elasticsearch\Tests\Unit\Query\Dsl\Term;
+
+use AviationCode\Elasticsearch\Query\Dsl\Term\Prefix;
+use AviationCode\Elasticsearch\Tests\Unit\TestCase;
+
+class PrefixTest extends TestCase
+{
+    /** @test **/
+    public function it_builds_prefix_clause()
+    {
+        $prefix = new Prefix('user', 'ki');
+
+        $this->assertEquals(['prefix' => ['user' => ['value' => 'ki']]], $prefix->toArray());
+    }
+
+    /** @test **/
+    public function it_builds_prefix_clause_with_rewrite()
+    {
+        $prefix = new Prefix('user', 'ki', 'constant_score');
+
+        $this->assertEquals(['prefix' => ['user' => [
+            'value' => 'ki',
+            'rewrite' => 'constant_score',
+        ]]], $prefix->toArray());
+    }
+}

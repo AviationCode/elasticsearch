7a10d5a3755cb9ce7a477cbeb607ceee0a03a772
Ken Andries <ken.andries.1992@gmail.com>
2019-12-29 10:24:10 +0100

    Add regexp query

---
+++ src/Query/Dsl/Term/Regexp.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AviationCode\Elasticsearch\Query\Dsl\Term;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+class Regexp implements Arrayable
+{
+    const KEY = 'regexp';
+
+    /**
+     * Field you wish to search for.
+     *
+     * @var string
+     */
+    private $field;
+
+    /**
+     * Regex expression.
+     *
+     * @var mixed
+     */
+    private $value;
+
+    /**
+     * Extra options given to elasticsearch.
+     * @see https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-regexp-query.html#regexp-query-field-params
+     *
+     * @var array
+     */
+    private $options;
+
+    /**
+     * Regexp constructor.
+     *
+     * @param string $field
+     * @param $value
+     * @param array $options
+     */
+    public function __construct(string $field, $value, array $options =[])
+    {
+        $this->field = $field;
+        $this->value = $value;
+        $this->options = $options;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toArray()
+    {
+        return [
+            static::KEY => [
+                $this->field => array_merge(['value' => $this->value], $this->options),
+            ],
+        ];
+    }
+}
---
+++ tests/Unit/Query/Dsl/Term/RegexpTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AviationCode\Elasticsearch\Tests\Unit\Query\Dsl\Term;
+
+use AviationCode\Elasticsearch\Query\Dsl\Term\Regexp;
+use AviationCode\Elasticsearch\Tests\Unit\TestCase;
+
+class RegexpTest extends TestCase
+{
+    /** @test **/
+    public function it_builds_simple_regexp()
+    {
+        $regexp = new Regexp('user', 'k.*y');
+
+        $this->assertEquals(['regexp' => ['user' => ['value' => 'k.*y']]], $regexp->toArray());
+    }
+
+    /** @test **/
+    public function it_builds_advanced_regexp()
+    {
+        $regexp = new Regexp('user', 'k.*y', [
+            'flags' => 'ALL',
+            'max_determinized_states' => 10000,
+            'rewrite' => 'constant_score',
+        ]);
+
+        $this->assertEquals(['regexp' => [
+            'user' => [
+                'value' => 'k.*y',
+                'flags' => 'ALL',
+                'max_determinized_states' => 10000,
+                'rewrite' => 'constant_score',
+            ],
+        ]], $regexp->toArray());
+    }
+}

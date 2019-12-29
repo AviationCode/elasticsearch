380aaed95e09db58367ee198aaeaab57581deb94
Ken Andries <ken.andries.1992@gmail.com>
2019-12-29 10:24:28 +0100

    Add terms query

---
+++ src/Query/Dsl/Term/Terms.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AviationCode\Elasticsearch\Query\Dsl\Term;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+class Terms implements Arrayable
+{
+    const KEY = 'terms';
+
+    /**
+     * The field you wish to search for.
+     *
+     * @var string
+     */
+    private $field;
+
+    /**
+     * Terms to search for.
+     *
+     * @var array
+     */
+    private $values;
+
+    /**
+     * Optionally boost factor.
+     *
+     * @var float|null
+     */
+    private $boost;
+
+    /**
+     * Terms constructor.
+     *
+     * @param string $field
+     * @param array $values
+     * @param float|null $boost
+     */
+    public function __construct(string $field, array $values, ?float $boost = null)
+    {
+        $this->field = $field;
+        $this->values = $values;
+        $this->boost = $boost;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toArray()
+    {
+        $params = [$this->field => $this->values];
+
+        if ($this->boost) {
+            $params['boost'] = $this->boost;
+        }
+
+        return [static::KEY => $params];
+    }
+}
---
+++ tests/Unit/Query/Dsl/Term/TermsTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AviationCode\Elasticsearch\Tests\Unit\Query\Dsl\Term;
+
+use AviationCode\Elasticsearch\Query\Dsl\Term\Terms;
+use AviationCode\Elasticsearch\Tests\Unit\TestCase;
+
+class TermsTest extends TestCase
+{
+    /** @test **/
+    public function it_builds_terms()
+    {
+        $terms = new Terms('user', ['kimchy', 'elasticsearch']);
+
+        $this->assertEquals([
+            'terms' => [
+                'user' => ['kimchy', 'elasticsearch'],
+            ],
+        ], $terms->toArray());
+    }
+
+    /** @test **/
+    public function it_builds_terms_with_boost()
+    {
+        $terms = new Terms('user', ['kimchy', 'elasticsearch'], 2.0);
+
+        $this->assertEquals([
+            'terms' => [
+                'user' => ['kimchy', 'elasticsearch'],
+                'boost' => 2.0,
+            ],
+        ], $terms->toArray());
+    }
+}

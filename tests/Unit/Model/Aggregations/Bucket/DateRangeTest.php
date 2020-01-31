47d1a83950897815ec881fea10575efa085a1438
Hamza Hejja <hamzahejja@hotmail.com>
2020-01-31 19:49:56 +0200

    feat(bucket aggs): implement Date Range Bucket Aggregation

---
+++ tests/Unit/Model/Aggregations/Bucket/DateRangeTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace AviationCode\Elasticsearch\Tests\Unit\Model\Aggregations\Bucket;
+
+use AviationCode\Elasticsearch\Model\Aggregations\Bucket\DateRange;
+use AviationCode\Elasticsearch\Query\Aggregations\Bucket\DateRange as DateRangeQuery;
+use AviationCode\Elasticsearch\Tests\Unit\TestCase;
+use Carbon\Carbon;
+
+class DateRangeTest extends TestCase
+{
+    /** @test */
+    public function it_successfully_translates_a_date_range()
+    {
+        $dateRange = new DateRange([
+            'buckets' => [
+                [
+                    'to' => 1.4436576E12,
+                    'to_as_string' => '10-2015',
+                    'doc_count' => 7,
+                    'key' => '*-10-2015',
+                ],
+                [
+                    'from' => 1.4436576E12,
+                    'from_as_string' => '10-2015',
+                    'doc_count' => 0,
+                    'key' => '10-2015-*',
+                ],
+            ],
+        ]);
+
+        $this->assertCount(2, $dateRange);
+        $this->assertEquals(7, $dateRange->first()->doc_count);
+        $this->assertInstanceOf(Carbon::class, $dateRange->first()->to);
+        $this->assertInstanceOf(Carbon::class, $dateRange->last()->from);
+    }
+
+    /** @test */
+    public function it_successfully_translates_a_date_range_with_keyed_response()
+    {
+        $dateRange = new DateRange([
+            'buckets' => [
+                '*-10-2015' => [
+                    'to' => 1.4436576E12,
+                    'to_as_string' => '10-2015',
+                    'doc_count' => 7,
+                ],
+                '10-2015-*' => [
+                    'from' => 1.4436576E12,
+                    'from_as_string' => '10-2015',
+                    'doc_count' => 0,
+                ],
+            ],
+        ]);
+
+        /**
+         * In case of keyed response, since
+         * Bucket extends \Illuminate\Support\Collection
+         * an array of buckets can be obtained by using: array_values helper
+         * with all() Collection method on the $dateRange instance.
+         * Because the $dateRange holds a collection of 'key' => bucket pairs.
+         * and even if NOT used, the value will returned/injected
+         * when using methods: first(), last() of the Collection class.
+         */
+        $buckets = array_values($dateRange->all());
+
+        $this->assertCount(2, $dateRange);
+        $this->assertEquals(7, $buckets[0]->doc_count);
+        $this->assertInstanceOf(Carbon::class, $buckets[0]->to);
+        $this->assertInstanceOf(Carbon::class, $buckets[1]->from);
+    }
+}

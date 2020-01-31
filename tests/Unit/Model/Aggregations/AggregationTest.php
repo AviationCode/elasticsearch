e38a7c5a617d1d3fd37681ffaea06bc4daee114e
Ken Andries <Ken.andries.1992@gmail.com>
2020-01-31 13:44:28 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Unit/Model/Aggregations/AggregationTest.php
+++ tests/Unit/Model/Aggregations/AggregationTest.php
@@ -17,18 +17,18 @@ class AggregationTest extends TestCase
                 'buckets' => [
                     [
                         'key' => 'electronic',
-                        'doc_count' => 6
+                        'doc_count' => 6,
                     ],
                     [
                         'key' => 'rock',
-                        'doc_count' => 3
+                        'doc_count' => 3,
                     ],
                     [
                         'key' => 'jazz',
-                        'doc_count' => 2
-                    ]
-                ]
-            ]
+                        'doc_count' => 2,
+                    ],
+                ],
+            ],
         ]);
 
         $this->assertSame(0, $aggregations->genres->doc_count_error_upper_bound);
@@ -51,18 +51,18 @@ class AggregationTest extends TestCase
                 'buckets' => [
                     [
                         'key' => 1,
-                        'doc_count' => 6
+                        'doc_count' => 6,
                     ],
                     [
                         'key' => 2,
-                        'doc_count' => 3
+                        'doc_count' => 3,
                     ],
                     [
                         'key' => 3,
-                        'doc_count' => 2
-                    ]
-                ]
-            ]
+                        'doc_count' => 2,
+                    ],
+                ],
+            ],
         ]);
 
         $this->assertSame(0, $aggregations->genres->doc_count_error_upper_bound);

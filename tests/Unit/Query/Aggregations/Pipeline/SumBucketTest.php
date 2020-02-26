8c795e49ffd9ed5faf649ceba88e58dc9b19be6c
Ken Andries <ken.andries.1992@gmail.com>
2020-02-26 21:21:23 +0100

    Add sum bucket aggregation

---
+++ tests/Unit/Query/Aggregations/Pipeline/SumBucketTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AviationCode\Elasticsearch\Tests\Unit\Query\Aggregations\Pipeline;
+
+use AviationCode\Elasticsearch\Query\Aggregations\Pipeline\SumBucket;
+use AviationCode\Elasticsearch\Tests\Unit\TestCase;
+
+class SumBucketTest extends TestCase
+{
+    /** @test **/
+    public function it_builds_sum_bucket_aggregation()
+    {
+        $bucket = new SumBucket('the_sum');
+
+        $this->assertEquals([
+            'sum_bucket' => [
+                'buckets_path' => 'the_sum',
+            ],
+        ], $bucket->toArray());
+    }
+
+    /** @test **/
+    public function it_builds_sum_bucket_aggregation_gap_policy()
+    {
+        $bucket = new SumBucket('the_sum', SumBucket::GAP_INSERT_ZEROS);
+
+        $this->assertEquals([
+            'sum_bucket' => [
+                'buckets_path' => 'the_sum',
+                'gap_policy' => 'insert_zeros',
+            ],
+        ], $bucket->toArray());
+
+        $bucket = new SumBucket('the_sum', SumBucket::GAP_SKIP);
+
+        $this->assertEquals([
+            'sum_bucket' => [
+                'buckets_path' => 'the_sum',
+                'gap_policy' => 'skip',
+            ],
+        ], $bucket->toArray());
+    }
+
+    /** @test **/
+    public function it_builds_sum_bucket_aggregation_format()
+    {
+        $bucket = new SumBucket('the_sum', null, '000.00');
+
+        $this->assertEquals([
+            'sum_bucket' => [
+                'buckets_path' => 'the_sum',
+                'format' => '000.00',
+            ],
+        ], $bucket->toArray());
+    }
+}
